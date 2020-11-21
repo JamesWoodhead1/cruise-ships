966b1611e4e9c183185b2d402536493904e1a828
James Woodhead <james_woodhead95@hotmail.co.uk>
2020-11-21 15:15:45 +0000

    Used DRY to make ship test suite neater

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -3,30 +3,27 @@ const Port = require('../src/port.js')
 const Itinerary = require('../src/itinerary.js')
 
 describe('Ship', () => {
+    describe('with ports and an itinerary', () => {
+        let hull;
+        let bergen;
+        let itinerary;
+        let ship;
+        beforeEach(() => {
+            hull = new Port('Hull');
+            bergen = new Port('Bergen');
+            itinerary = new Itinerary([hull, bergen]);
+            ship = new Ship(itinerary);
+        })
     it('returns an Object', () => {
-        const port = new Port('Hull');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
-
         expect(ship).toBeInstanceOf(Object);
     });
     it('Has a starting port', () => {
-        const port = new Port('Hull');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
-
-        expect(ship.currentPort).toBe(port);
+        expect(ship.currentPort).toBe(hull);
     });
     it('gets added to port when instanced', () => {
-        const hull = new Port('Hull');
-        const itinerary = new Itinerary([hull]);
-        const ship = new Ship(itinerary);
-
         expect(hull.ships).toContain(ship);
-    })
+    });
 });
-
-describe('setSail', () => {
     it('can set sail', () => {
         const hull = new Port('Hull');
         const bergen = new Port('Bergen');
@@ -38,15 +35,12 @@ describe('setSail', () => {
         expect(ship.currentPort).toBeFalsy();
         expect(hull.ships).not.toContain(ship);
     });
-});
-
-describe('dockShip', () => {
     it('can dock at a different port', () => {
         const hull = new Port('Hull');
         const bergen = new Port('Bergen');
-        const itinerary = new Itinerary([hull, bergen])
+        const itinerary = new Itinerary([hull, bergen]);
         const ship = new Ship(itinerary);
-        
+
         ship.setSail();
         ship.dockShip();
 
@@ -56,7 +50,7 @@ describe('dockShip', () => {
     it('cannot sail beyond its itinerary', () => {
         const hull = new Port('Hull');
         const bergen = new Port('Bergen');
-        const itinerary = new Itinerary([hull, bergen])
+        const itinerary = new Itinerary([hull, bergen]);
         const ship = new Ship(itinerary);
         
         ship.setSail();
